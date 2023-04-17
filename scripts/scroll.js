2129f17f4b681f11dfffd6051393687a576b6be3
Kalvin <klvpva@gmail.com>
2023-04-17 16:45:47 +0200

    Added body background color change

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -10,8 +10,25 @@ for(scrollApear of scrollApears){
     scrollApear.checkPosition();
 }
 
+scrollBackgrounds = document.getElementsByClassName("scroll-bg-change");
+for(scrollBackground of scrollBackgrounds){
+    scrollBackground.checkPosition = function(){
+        rect = scrollBackground.getBoundingClientRect();
+        console.log(rect.top);
+        if(rect.top >= -window.innerHeight*.5 && rect.top < window.innerHeight*.5){
+            //console.log("ehhho");
+            document.body.style.backgroundColor = this.getAttribute("scroll-bg-color");
+        }
+    }
+    scrollBackground.checkPosition();
+}
+
+
 document.addEventListener("scroll", (event) => {
     for(scrollApear of scrollApears){
         scrollApear.checkPosition();
+    }
+    for(scrollBackground of scrollBackgrounds){
+        scrollBackground.checkPosition();
     }
 });
